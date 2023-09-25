4a50def84558a2be10310ca770910d5cd78e000b
Julian Tellez <jtellez9318@gmail.com>
2023-09-24 21:57:47 -0500

    Fixing setter.py

--- utilities/setter.py
+++ utilities/setter.py
@@ -2,10 +2,13 @@ import os
 
 from base64 import b64decode
 
+import os
+import json
+
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
     with open('path.json', 'w') as json_file:
-        json_file.write(b64decode(key.decode()))
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath('path.json'))
 
 if __name__ == '__main__':
